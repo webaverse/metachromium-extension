5962b1281ec7c40aed45fb5d398c165caf7a4bf3
Avaer Kazmer <a@modules.io>
2020-01-04 19:00:35 -0500

    Hook in new settings proxy in BaseSettings

--- device/vr/OpenOVR/Reimpl/BaseSettings.cpp
+++ device/vr/OpenOVR/Reimpl/BaseSettings.cpp
@@ -16,50 +16,50 @@ using namespace std;
 
 const char * BaseSettings::GetSettingsErrorNameFromEnum(EVRSettingsError eError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::GetSettingsErrorNameFromEnum" << std::endl; });
-  return g_vrsettings->GetSettingsErrorNameFromEnum(eError);
+  return g_pvrsettings->GetSettingsErrorNameFromEnum(eError);
 }
 bool  BaseSettings::Sync(bool bForce, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::Sync" << std::endl; });
-	return g_vrsettings->Sync(bForce, peError);
+	return g_pvrsettings->Sync(bForce, peError);
 }
 void  BaseSettings::SetBool(const char * pchSection, const char * pchSettingsKey, bool bValue, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::SetBool" << std::endl; });
-	return g_vrsettings->SetBool(pchSection, pchSettingsKey, bValue, peError);
+	return g_pvrsettings->SetBool(pchSection, pchSettingsKey, bValue, peError);
 }
 void  BaseSettings::SetInt32(const char * pchSection, const char * pchSettingsKey, int32_t nValue, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::SetInt32" << std::endl; });
-	return g_vrsettings->SetInt32(pchSection, pchSettingsKey, nValue, peError);
+	return g_pvrsettings->SetInt32(pchSection, pchSettingsKey, nValue, peError);
 }
 void  BaseSettings::SetFloat(const char * pchSection, const char * pchSettingsKey, float flValue, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::SetFloat" << std::endl; });
-	return g_vrsettings->SetFloat(pchSection, pchSettingsKey, flValue, peError);
+	return g_pvrsettings->SetFloat(pchSection, pchSettingsKey, flValue, peError);
 }
 void  BaseSettings::SetString(const char * pchSection, const char * pchSettingsKey, const char * pchValue, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::SetString" << std::endl; });
-	return g_vrsettings->SetString(pchSection, pchSettingsKey, pchValue, peError);
+	return g_pvrsettings->SetString(pchSection, pchSettingsKey, pchValue, peError);
 }
 bool  BaseSettings::GetBool(const char * pchSection, const char * pchSettingsKey, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::GetBool" << std::endl; });
-	return g_vrsettings->GetBool(pchSection, pchSettingsKey, peError);
+	return g_pvrsettings->GetBool(pchSection, pchSettingsKey, peError);
 }
 int32_t  BaseSettings::GetInt32(const char * pchSection, const char * pchSettingsKey, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::GetInt32" << std::endl; });
-	return g_vrsettings->GetInt32(pchSection, pchSettingsKey, peError);
+	return g_pvrsettings->GetInt32(pchSection, pchSettingsKey, peError);
 }
 float  BaseSettings::GetFloat(const char * pchSection, const char * pchSettingsKey, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::GetFloat" << std::endl; });
-	return g_vrsettings->GetFloat(pchSection, pchSettingsKey, peError);
+	return g_pvrsettings->GetFloat(pchSection, pchSettingsKey, peError);
 }
 void  BaseSettings::GetString(const char * pchSection, const char * pchSettingsKey, VR_OUT_STRING() char * pchValue,
 	   uint32_t unValueLen, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::GetString" << std::endl; });
-  return g_vrsettings->GetString(pchSection, pchSettingsKey, pchValue, unValueLen, peError);
+  return g_pvrsettings->GetString(pchSection, pchSettingsKey, pchValue, unValueLen, peError);
 }
 void  BaseSettings::RemoveSection(const char * pchSection, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::RemoveSection" << std::endl; });
-	return g_vrsettings->RemoveSection(pchSection, peError);
+	return g_pvrsettings->RemoveSection(pchSection, peError);
 }
 void  BaseSettings::RemoveKeyInSection(const char * pchSection, const char * pchSettingsKey, EVRSettingsError * peError) {
   TRACE("BaseSettings", []() { getOut() << "BaseSettings::RemoveKeyInSection" << std::endl; });
-	return g_vrsettings->RemoveKeyInSection(pchSection, pchSettingsKey, peError);
+	return g_pvrsettings->RemoveKeyInSection(pchSection, pchSettingsKey, peError);
 }
