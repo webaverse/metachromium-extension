cb0f03d4a6c30135f1d05a49cba5381dfc5bcf68
Avaer Kazmer <a@modules.io>
2020-01-23 14:43:29 -0500

    Add base extended display tracing

--- device/vr/OpenOVR/Reimpl/BaseExtendedDisplay.cpp
+++ device/vr/OpenOVR/Reimpl/BaseExtendedDisplay.cpp
@@ -10,13 +10,16 @@ using namespace vr;
 
 // Unreal Engine uses this for some silly reason
 void BaseExtendedDisplay::GetWindowBounds(int32_t * pnX, int32_t * pnY, uint32_t * pnWidth, uint32_t * pnHeight) {
+  TRACE("BaseExtendedDisplay", []() { getOut() << "BaseExtendedDisplay::GetWindowBounds" << std::endl; });
 	g_vrextendeddisplay->GetWindowBounds(pnX, pnY, pnWidth, pnHeight);
 }
 
 void BaseExtendedDisplay::GetEyeOutputViewport(vr::EVREye eEye, uint32_t * pnX, uint32_t * pnY, uint32_t * pnWidth, uint32_t * pnHeight) {
+  TRACE("BaseExtendedDisplay", []() { getOut() << "BaseExtendedDisplay::GetEyeOutputViewport" << std::endl; });
 	g_vrextendeddisplay->GetEyeOutputViewport(eEye, pnX, pnY, pnWidth, pnHeight);
 }
 
 void BaseExtendedDisplay::GetDXGIOutputInfo(int32_t * pnAdapterIndex, int32_t * pnAdapterOutputIndex) {
+  TRACE("BaseExtendedDisplay", []() { getOut() << "BaseExtendedDisplay::GetDXGIOutputInfo" << std::endl; });
 	g_vrextendeddisplay->GetDXGIOutputInfo(pnAdapterIndex, pnAdapterOutputIndex);
 }
