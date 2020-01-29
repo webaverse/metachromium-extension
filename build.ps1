23aef1baef8be9003d8d596a73e12e3931b622f5
Avaer Kazmer <a@modules.io>
2020-01-29 02:05:40 -0500

    Build debugging

--- build.ps1
+++ build.ps1
@@ -16,7 +16,7 @@ ls
 cd device\vr
 mkdir build
 cd build
-vswhere
+ls C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise
 cmake -G "Visual Studio 15 2017" -DCMAKE_GENERATOR_PLATFORM=x64 -DCMAKE_BUILD_TYPE=Release ..
 msbuild /p:Configuration=Release /t:Clean ALL_BUILD.vcxproj
 msbuild /p:Configuration=Release ALL_BUILD.vcxproj
