2a5aaa8926b30ca2bcf716a7bf256fd36c13cc1d
Avaer Kazmer <a@modules.io>
2020-01-29 01:09:04 -0500

    More build debugging

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ echo building...
 $client = new-object System.Net.WebClient
 $client.DownloadFile("https://chromium.woolyss.com/f/chrlauncher-win64-stable-codecs-sync.zip", "chromium.zip")
 Expand-Archive -Path chromium.zip
+cd chromium
 ls
 
 echo done
