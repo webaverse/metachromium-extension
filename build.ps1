7d332ce08cfb401c305e93ef5b61dbb5728252b4
Avaer Kazmer <a@modules.io>
2020-01-29 01:25:55 -0500

    Debugging chrome build script

--- build.ps1
+++ build.ps1
@@ -2,8 +2,8 @@
 echo building...
 
 $client = new-object System.Net.WebClient
-$client.DownloadFile("https://chromium.woolyss.com/f/chrlauncher-win64-stable-codecs-sync.zip", "chromium.zip")
-Expand-Archive -Path chromium.zip
+$client.DownloadFile("https://github.com/Hibbiki/chromium-win64/releases/download/v79.0.3945.130-r706915/chrome.sync.7z", "chrome.7z")
+7z x chrome.7z
 cd chromium
 ls
 
