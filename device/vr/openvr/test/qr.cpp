5a2d1d930815847ead5a9a08183a20eb700dde9f
Avaer Kazmer <a@modules.io>
2020-02-02 08:15:06 -0500

    Add qr code location logging

--- device/vr/openvr/test/qr.cpp
+++ device/vr/openvr/test/qr.cpp
@@ -19,13 +19,24 @@ void detectQrCodes() {
   Mat bbox, rectifiedImage;
  
   std::string data = qrDecoder.detectAndDecode(inputImage, bbox, rectifiedImage);
-  if(data.length()>0)
+  if(data.length() > 0)
   {
-    getOut() << "Decoded Data : " << data << std::endl;
- 
-    // display(inputImage, bbox);
-    // rectifiedImage.convertTo(rectifiedImage, CV_8UC3);
-    // imshow("Rectified QRCode", rectifiedImage);
+    if (bbox.type() == CV_32FC2 && bbox.rows == 4 && bbox.cols == 1) {
+      Point2f points[4] = {
+        bbox.at<Point2f>(0),
+        bbox.at<Point2f>(1),
+        bbox.at<Point2f>(2),
+        bbox.at<Point2f>(3),
+      };
+      getOut() << "Decoded Data : " << data << " " <<
+        points[0].x << " " << points[0].y << " " <<
+        points[1].x << " " << points[1].y << " " <<
+        points[2].x << " " << points[2].y << " " <<
+        points[3].x << " " << points[3].y << " " <<
+        std::endl;
+    } else {
+      getOut() << "unknown qr code type: " << bbox.type() << std::endl;
+    }
   } else {
     getOut() << "QR Code not detected" << std::endl;
   }
