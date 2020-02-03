dfc04102bcb929f55e0f14a5c9c317dd00d2b1d9
Avaer Kazmer <a@modules.io>
2020-02-03 02:37:23 -0500

    Bugfix missing matrix.h include

--- device/vr/openvr/test/matrix.h
+++ device/vr/openvr/test/matrix.h
@@ -1,6 +1,8 @@
 #ifndef _openvr_matrix_h_
 #define _openvr_matrix_h_
 
+#include "third_party/openvr/src/headers/openvr.h"
+
 void setPoseMatrix(float *dstMatrixArray, const vr::HmdMatrix34_t &srcMatrix);
 void getMatrixInverse(const float *inMatrix, float *outMatrix);
 void multiplyMatrices(const float *aMatrix, const float *bMatrix, float *outMatrix);
