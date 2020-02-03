6f93dd41a5fe02c757ba8523330589de83aae933
Avaer Kazmer <a@modules.io>
2020-02-03 02:58:45 -0500

    New process4 setTransform command

--- device/vr/openvr/test/process4.cpp
+++ device/vr/openvr/test/process4.cpp
@@ -36,9 +36,11 @@ void respond(const json &j) {
 }
 
 int main(int argc, char **argv) {
+  g_fnp = new FnProxy();
+
   freopen(NULL, "rb", stdin);
   freopen(NULL, "wb", stdout);
-  
+
   char cwdBuf[MAX_PATH];
   if (!GetCurrentDirectory(
     sizeof(cwdBuf),
@@ -172,6 +174,45 @@ int main(int argc, char **argv) {
             HANDLE processHandle = OpenProcess(PROCESS_ALL_ACCESS, false, processId);
             TerminateProcess(processHandle, 1);
             CloseHandle(processHandle);
+
+            json res = {
+              {"error", nullptr},
+              {"result", "ok"}
+            };
+            respond(res);
+          } else if (
+            methodString == "setTransform" &&
+            args.size() >= 3 &&
+            args[0].is_array() && args[0].size() >= 3 && args[0][0].is_number() && args[0][1].is_number() && args[0][2].is_number() &&
+            args[1].is_array() && args[1].size() >= 3 && args[1][0].is_number() && args[1][1].is_number() && args[1][2].is_number() && args[1][3].is_number() &&
+            args[2].is_array() && args[2].size() >= 3 && args[2][0].is_number() && args[2][1].is_number() && args[2][2].is_number()
+          ) {
+            managed_binary<float> position(3);
+            float *positionData = (float *)position.data();
+            positionData[0] = args[0][0].get<float>();
+            positionData[1] = args[0][1].get<float>();
+            positionData[2] = args[0][2].get<float>();
+
+            managed_binary<float> quaternion(4);
+            float *quaternionData = (float *)quaternion.data();
+            quaternionData[0] = args[1][0].get<float>();
+            quaternionData[1] = args[1][1].get<float>();
+            quaternionData[2] = args[1][2].get<float>();
+            quaternionData[3] = args[1][3].get<float>();
+
+            managed_binary<float> scale(3);
+            float *scaleData = (float *)scale.data();
+            scaleData[0] = args[2][0].get<float>();
+            scaleData[1] = args[2][1].get<float>();
+            scaleData[2] = args[2][2].get<float>();
+
+            auto result = g_fnp.call<
+              kIVRCompositor_SetTransform,
+              int,
+              managed_binary<float>,
+              managed_binary<float>,
+              managed_binary<float>
+            >(std::move(position), std::move(quaternion), std::move(scale));
 
             json res = {
               {"error", nullptr},
