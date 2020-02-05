37f659fcb6d18efb3f0983bb3ac22f6b80fcc8ff
Avaer Kazmer <a@modules.io>
2020-02-05 06:29:31 -0500

    Small build script cleanup

--- build.ps1
+++ build.ps1
@@ -1,8 +1,9 @@
 # build
 echo building...
 
-$client1 = new-object System.Net.WebClient
-$client1.DownloadFile("https://github.com/Hibbiki/chromium-win64/releases/download/v79.0.3945.130-r706915/chrome.sync.7z", "chrome.7z")
+$ProgressPreference = 'SilentlyContinue'
+
+Invoke-WebRequest "https://github.com/Hibbiki/chromium-win64/releases/download/v79.0.3945.130-r706915/chrome.sync.7z" -OutFile "chrome.7z"
 7z x chrome.7z
 rm chrome.7z
 
