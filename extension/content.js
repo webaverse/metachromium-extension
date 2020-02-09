295aa4c6564fae7501b37e41f84483bc06678811
Avaer Kazmer <a@modules.io>
2020-02-09 11:29:19 -0500

    Content.js cleanup

--- extension/content.js
+++ extension/content.js
@@ -39,20 +39,14 @@ window.addEventListener('message', m => {
 });
 window.addEventListener('load', async () => {
   window.dispatchEvent(new MessageEvent('xrchromeloaded'));
-  console.log('load 1');
   const _keydown = e => {
-    console.log('got keydown', e.ctrlKey, e.which);
     if (e.ctrlKey && e.which === 17) {
-      console.log('emit 1');
       window.removeEventListener('keydown', _keydown);
       navigator.xr.dispatchEvent(new MessageEvent('activate'));
-      console.log('emit 2');
     }
   };
   window.addEventListener('keydown', _keydown);
-  console.log('load 2');
   await window.xrchrome.request('activate', []);
-  console.log('load 3');
 });
 
 /* window.xrchrome.request('test', [])
