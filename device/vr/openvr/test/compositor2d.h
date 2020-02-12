207325f92df0901060d3698cddd6e01db43fe545
Avaer Kazmer <a@modules.io>
2020-02-11 22:00:32 -0500

    Re-add compositor2d to build

---
+++ device/vr/openvr/test/compositor2d.h
@@ -0,0 +1,22 @@
+#ifndef _openvr_compositor2d_h_
+#define _openvr_compositor2d_h_
+
+#include <D3D11_4.h>
+#include <d3dcompiler.h>
+#include <DXGI1_4.h>
+#include <wrl.h>
+
+// #include "third_party/openvr/src/headers/openvr.h"
+#include "device/vr/openvr/test/out.h"
+
+namespace vr {
+  class PVRCompositor;
+}
+
+namespace compositor2d {
+
+void homeRenderLoop();
+
+}
+
+#endif
